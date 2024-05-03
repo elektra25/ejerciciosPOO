192b312217e7498ad6c295a224dbd844c8e0ecdf
elektra25 <eloasxdgamer04@gmail.com>
2024-05-03 11:59:59 -0600

    Trabajos Get and Set

---
+++ MainPersona.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class MainPersona {
+    public static void main (String [] args){
+        String nombre,apellido;
+        int edad;
+        double altura;
+        Scanner op = new Scanner (System.in);
+        Persona paco = new Persona ();
+        System.out.println ("¿Cual es tu nombre?");
+        nombre=op.nextLine();
+        paco.setNombre(nombre);
+        System.out.println ("¿Cual es tu apellido?");
+        apellido=op.nextLine();
+        paco.setApellido(apellido);
+        System.out.println ("¿Cual es tu edad?");
+        edad=op.nextInt();
+        paco.setEdad(edad);
+        System.out.println ("¿Cual es tu altura?");
+        altura=op.nextDouble();
+        paco.setAltura(altura);
+        
+        System.out.println(paco);
+
+
+        
+    }
+
+   
+    }
